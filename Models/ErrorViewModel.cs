43df605f0634df4792f8c8933a5c1f16390f6e0d
Rajesh PA <rajurajesh125@outlook.com>
2021-02-07 21:27:47 +0530

    new

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mysecondwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
